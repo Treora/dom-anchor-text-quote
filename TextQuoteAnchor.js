b7e82175a20d0a1f4f970af5f1b81965fd442548
Gerben <gerben@treora.com>
2016-03-06 13:38:47 +0100

    Replace diff-match-patch with RegExp.
    Now only whitespace is matched in a fuzzy way.

--- TextQuoteAnchor.js
+++ TextQuoteAnchor.js
@@ -1,7 +1,5 @@
-import DiffMatchPatch from 'diff-match-patch';
 import TextPositionAnchor from 'dom-anchor-text-position';
 
-// The DiffMatchPatch bitap has a hard 32-character pattern length limit.
 const CONTEXT_LENGTH = 32;
 
 
@@ -62,63 +60,23 @@ export default class TextQuoteAnchor {
   }
 
   toPositionAnchor(options = {}) {
-    let {hint} = options;
     let root = this.root;
-    let dmp = new DiffMatchPatch();
+    let text = root.textContent;
 
-    dmp.Match_Distance = root.textContent.length * 2;
-
-    // Work around a hard limit of the DiffMatchPatch bitap implementation.
-    // The search pattern must be no more than 32 characters.
-    let slices = this.exact.match(/(.|[\r\n]){1,32}/g);
-    let loc = (hint === undefined) ? ((root.textContent.length / 2) | 0) : hint;
-    let start = Number.POSITIVE_INFINITY;
-    let end = Number.NEGATIVE_INFINITY;
-    let result = -1;
-
-    // If the prefix is known then search for that first.
-    if (this.prefix !== undefined) {
-      result = dmp.match_main(root.textContent, this.prefix, loc);
-      if (result > -1) loc = result + this.prefix.length;
+    // Whitespace is whitespace, do not fuss about types and amounts of it.
+    function spaceInsensitiveRegExp(string) {
+        let regex = string.replace(/\s+/g, '\\s+');
+        return new RegExp(regex, 'g');
     }
+    let pattern = spaceInsensitiveRegExp(this.exact);
 
-    // Search for the first slice.
-    let firstSlice = slices.shift();
-    result = dmp.match_main(root.textContent, firstSlice, loc);
-    if (result > -1) {
-      start = result;
-      loc = end = start + firstSlice.length;
-    } else {
-      throw new Error('no match found');
-    }
-
-    // Create a fold function that will reduce slices to positional extents.
-    let foldSlices = (acc, slice) => {
-      let result = dmp.match_main(root.textContent, slice, acc.loc);
-      if (result === -1) {
+    // Search for the pattern.
+    let start = text.search(pattern);
+    if (start === -1) {
         throw new Error('no match found');
-      }
+    }
+    let end = start + text.match(pattern)[0].length;
 
-      // The next slice should follow this one closely.
-      acc.loc = result + slice.length;
-
-      // Expand the start and end to a quote that includes all the slices.
-      acc.start = Math.min(acc.start, result);
-      acc.end = Math.max(acc.end, result + slice.length);
-
-      return acc;
-    };
-
-    // Use the fold function to establish the full quote extents.
-    // Expect the slices to be close to one another.
-    // This distance is deliberately generous for now.
-    dmp.Match_Distance = 64;
-    let acc = slices.reduce(foldSlices, {
-      start: start,
-      end: end,
-      loc: loc,
-    });
-
-    return new TextPositionAnchor(root, acc.start, acc.end);
+    return new TextPositionAnchor(root, start, end);
   }
 }
